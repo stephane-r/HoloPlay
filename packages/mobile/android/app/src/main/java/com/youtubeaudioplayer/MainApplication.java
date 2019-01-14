f8eb745afdc18851eb5bcaf19e2020e5030b3e55
stephane <stephane@novaway.fr>
2019-01-14 17:10:34 +0100

    ➕ Add react-native-music-control dependency

--- packages/mobile/android/app/src/main/java/com/youtubeaudioplayer/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/youtubeaudioplayer/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import com.brentvatne.react.ReactVideoPackage;
+import com.tanguyantoine.react.MusicControl;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new MusicControl(),
           new MainReactPackage(),
           new ReactVideoPackage()
       );
