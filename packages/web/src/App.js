8c23685c2777fe1e91a2f9b706194a491620bcc6
stephane <stephane@novaway.fr>
2019-01-17 11:53:08 +0100

    💄 Update web App views

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,31 +1,52 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import test from "workspace-b";
+import React, { Component } from 'react';
+import { StyleSheet, View, ScrollView, Button } from 'react-native';
+// import Input from './components/Forms/Input';
+import {
+  Provider,
+  actions,
+  SearchResultContainer
+} from '@youtube-audio-player/core';
 
-test();
+export default class App extends Component {
+  componentDidMount() {
+    actions.search();
+  }
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider>
+        <ScrollView>
+          <View style={styles.container}>
+            {/* <Input
+              onChangeText={text => actions.setSearchValue(text)}
+              placeholder="Rechercher..."
+            /> */}
+            <Button title="Search" onPress={actions.search} />
+            <SearchResultContainer
+              onPress={index => actions.loadSource(index)}
+            />
+          </View>
+        </ScrollView>
+      </Provider>
     );
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
