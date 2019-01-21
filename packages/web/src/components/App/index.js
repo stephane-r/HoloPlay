fbc20d10e857e0a5c7274951b1284b52aa3d208b
stephane <stephane@novaway.fr>
2019-01-21 09:41:12 +0100

    ✨ Init navigation for web app

--- packages/web/src/components/App/index.js
+++ packages/web/src/components/App/index.js
@@ -1,90 +1,45 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, Button } from 'react-native';
-import { Input } from '@youtube-audio-player/components';
+import React from 'react';
+import { ScrollView } from 'react-native';
 import {
-  Provider,
-  actions,
-  SearchResultContainer
-} from '@youtube-audio-player/core';
-import AudioContainer from '../../containers/Audio';
+  createNavigator,
+  SwitchRouter,
+  SceneView
+} from '@react-navigation/core';
+import { createBrowserApp, Link } from '@react-navigation/web';
+import { Provider } from '@youtube-audio-player/core';
+import DashboardScreen from '../../screens/Dashboard';
+import LoginScreen from '../../screens/Login';
 
-class App extends Component {
-  state = {
-    identifier: 'contact@stephane-richin.fr',
-    password: 'steph0407'
-  };
-
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  componentDidMount() {
-    actions.search();
-  }
-
-  async handleChange(key, value) {
-    await this.setState({
-      [key]: value
-    });
-
-    console.log(this.state);
-  }
-
+class Layout extends React.Component {
   render() {
+    const { descriptors, navigation } = this.props;
+    const activeKey = navigation.state.routes[navigation.state.index].key;
+    const descriptor = descriptors[activeKey];
+
     return (
       <Provider>
         <ScrollView>
-          <View>
-            <Input
-              onChangeText={value => this.handleChange('identifier', value)}
-              placeholder="identifier"
-              value={this.state.identifier}
-            />
-            <Input
-              onChangeText={value => this.handleChange('password', value)}
-              placeholder="Password"
-              value={this.state.password}
-            />
-            <Button
-              title="Login"
-              onPress={() => actions.loginThroughApi(this.state)}
-            />
-          </View>
-          <View style={styles.container}>
-            <Input
-              onChangeText={text => actions.setSearchValue(text)}
-              placeholder="Rechercher..."
-            />
-            <Button title="Search" onPress={actions.search} />
-            <SearchResultContainer
-              onPress={index => actions.loadSource(index)}
-            />
-            <AudioContainer />
-          </View>
+          <Link routeName="LoginScreen">Login</Link>
+          <Link routeName="DashboardScreen">Dashboard</Link>
+          <SceneView
+            component={descriptor.getComponent()}
+            navigation={descriptor.navigation}
+          />
         </ScrollView>
       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
+const AppNavigator = createNavigator(
+  Layout,
+  SwitchRouter({
+    LoginScreen,
+    DashboardScreen
+  }),
+  {}
+);
+
+const App = createBrowserApp(AppNavigator);
 
 export default App;
---
+++ packages/web/src/screens/Login/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+import { Input } from '@youtube-audio-player/components';
+import { actions } from '@youtube-audio-player/core';
+
+class Login extends React.Component {
+  static path = '';
+
+  static navigationOptions = () => ({
+    title: 'Login',
+    linkName: 'Login'
+  });
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  state = {
+    identifier: 'contact@stephane-richin.fr',
+    password: 'steph0407'
+  };
+
+  async handleChange(key, value) {
+    await this.setState({
+      [key]: value
+    });
+
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <View>
+        <Input
+          onChangeText={value => this.handleChange('identifier', value)}
+          placeholder="identifier"
+          value={this.state.identifier}
+        />
+        <Input
+          onChangeText={value => this.handleChange('password', value)}
+          placeholder="Password"
+          value={this.state.password}
+        />
+        <Button
+          title="Login"
+          onPress={() => actions.loginThroughApi(this.state)}
+        />
+      </View>
+    );
+  }
+}
+
+export default Login;
