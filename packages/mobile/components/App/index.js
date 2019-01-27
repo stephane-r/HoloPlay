f1dc62b3e929449963781a62ed298e791e40c9b5
Stéphane <stephane@novaway.fr>
2019-01-27 17:54:20 +0100

    :sparkles: Update mobile navigation with Favoris screen

--- packages/mobile/components/App/index.js
+++ packages/mobile/components/App/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { AsyncStorage, ActivityIndicator } from 'react-native';
 import { Provider } from '@youtube-audio-player/core';
-import SwitchNavigator from '../../navigation/SwitchNavigator';
+import { createRootNavigator } from '../../navigation/TabNavigator';
 
 class App extends React.Component {
+  state = {
+    checkedLogged: false,
+    isLogged: false
+  };
+
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem('userToken');
+
+    if (token) {
+      this.setState({
+        isLogged: true,
+        checkedLogged: true
+      });
+    }
+  }
+
   render() {
+    const { checkedLogged, isLogged } = this.state;
+    const Layout = createRootNavigator(isLogged);
+
+    if (!checkedLogged) {
+      return <ActivityIndicator />;
+    }
+
     return (
       <Provider>
-        <SwitchNavigator />
+        <Layout />
       </Provider>
     );
   }
---
+++ packages/mobile/screens/Favoris/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { actions, FavorisContainer } from '@youtube-audio-player/core';
+// import AudioContainer from '../../containers/Audio';
+
+class Favoris extends React.Component {
+  static path = 'favoris';
+
+  static navigationOptions = () => ({
+    title: 'Favoris',
+    linkName: 'Favoris'
+  });
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.logout}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+        <FavorisContainer onPress={index => actions.loadSource(index)} />
+        {/* <AudioContainer /> */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
+
+export default Favoris;
