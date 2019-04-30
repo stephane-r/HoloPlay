436c6ec3ee8e4596d0fe5fa3abfd0ef4fc6dd613
Stéphane <stephane@novaway.fr>
2019-04-30 09:02:40 +0200

    :bento: Add DINPro Regular and Bold fonts

--- storybook/stories.js
+++ storybook/stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const style = {
   flex: 1,
@@ -11,10 +11,28 @@ const style = {
 
 const CenteredView = ({ children }) => <View style={style}>{children}</View>;
 
+const AppText = ({ children, bold }) => (
+  <Text style={[styles.text, bold && styles.textBold]}>{children}</Text>
+);
+
+AppText.defaultProps = {
+  bold: false
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'DINPro-Regular'
+  },
+  textBold: {
+    fontFamily: 'DINPro-Bold'
+  }
+});
+
 storiesOf('CenteredView', module)
   .add('default view', () => (
     <CenteredView>
-      <Text>Hello Storybook</Text>
+      <AppText>Hello Storybook</AppText>
+      <AppText bold>Hello bold Storybook</AppText>
     </CenteredView>
   ))
   .add('default vieww', () => (
