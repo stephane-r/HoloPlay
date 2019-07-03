7066fa756d5bd796e1ec6e4b62e71d6987910a52
Stéphane <stephane@novaway.fr>
2019-07-03 17:46:03 +0200

    :lipstick: Test sidebar for add player

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,27 +4,73 @@ import {
   ScrollView,
   View,
   StyleSheet,
-  DrawerLayoutAndroid
+  Animated,
+  DrawerLayoutAndroid,
+  Dimensions
 } from 'react-native';
+import { Text, Button } from 'react-native-paper';
 import DrawlerContainer from '../../containers/Drawler';
+
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const FROM_VALUE = WINDOW_WIDTH;
+const TO_VALUE = 0;
 
 type LayoutProps = {
   navigation: Object,
   children: React.Node
 };
 
-const Layout = ({ navigation, children }: LayoutProps) => (
-  <DrawerLayoutAndroid
-    drawerWidth={300}
-    drawerPosition={DrawerLayoutAndroid.positions.Left}
-    renderNavigationView={() => <DrawlerContainer navigation={navigation} />}>
-    <View style={{ flex: 1 }}>
-      <ScrollView style={{ flex: 1 }}>
-        <View style={styles.container}>{children}</View>
-      </ScrollView>
-    </View>
-  </DrawerLayoutAndroid>
-);
+const Drawer = ({ visible, close }) => {
+  const from = visible ? TO_VALUE : FROM_VALUE;
+  const to = visible ? FROM_VALUE : TO_VALUE;
+
+  const animatedValue = new Animated.Value(from);
+
+  Animated.timing(animatedValue, {
+    useNativeDriver: true,
+    toValue: to
+  }).start();
+
+  return (
+    <Animated.View
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: Dimensions.get('window').height,
+        zIndex: 2,
+        backgroundColor: 'red',
+        transform: [{ translateX: animatedValue }]
+      }}>
+      <Text>Hello from drawer !</Text>
+      <Button onPress={close}>Close drawer</Button>
+    </Animated.View>
+  );
+};
+
+const Layout = ({ navigation, children }: LayoutProps) => {
+  const [visible, setIsVisible] = React.useState(false);
+
+  const toggleDrawer = () => setIsVisible(!visible);
+
+  return (
+    <DrawerLayoutAndroid
+      drawerWidth={300}
+      drawerPosition={DrawerLayoutAndroid.positions.Left}
+      renderNavigationView={() => <DrawlerContainer navigation={navigation} />}>
+      <View style={{ flex: 1 }}>
+        <Drawer
+          visible={visible}
+          close={toggleDrawer} />
+        <ScrollView style={{ flex: 1 }}>
+          <Button onPress={toggleDrawer}>Open drawer</Button>
+          <View style={styles.container}>{children}</View>
+        </ScrollView>
+      </View>
+    </DrawerLayoutAndroid>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
