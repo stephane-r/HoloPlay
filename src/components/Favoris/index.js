52f0dbd880f6a53ca9c5b77069fcc4dd6787d314
Stéphane <stephane@novaway.fr>
2019-08-02 15:44:38 +0200

    :recycle: Refactoring Sidebar and Player on open event

---
+++ src/components/Favoris/index.js
@@ -0,0 +1,90 @@
+// @flow
+import React from 'react';
+import { IconButton, Button } from 'react-native-paper';
+import { useQuery } from 'react-apollo-hooks';
+import { withApollo } from 'react-apollo';
+import GET_USER from '../../graphql/query/user';
+import { ADD_TO_FAVORIS } from '../../graphql/mutation/favoris';
+
+type FavorisProps = {
+  client: Object,
+  userId: number,
+  source: Object,
+  buttonWithIcon: boolean
+};
+
+const Favoris = ({ client, userId, source, buttonWithIcon }) => {
+  const { data, error, loading } = useQuery(GET_USER, {
+    variables: { userId }
+  });
+
+  if (loading || error) {
+    return null;
+  }
+
+  const AddOrRemoveToFavoris = () => {
+    const refetchQueries = [
+      {
+        query: GET_USER,
+        variables: { userId }
+      }
+    ];
+
+    const favorisIds = data.user.favorisIds ? data.user.favorisIds : [];
+    const favoris = data.user.favoris ? data.user.favoris : [];
+
+    if (isFavoris) {
+      return client.mutate({
+        mutation: ADD_TO_FAVORIS,
+        variables: {
+          userId,
+          favorisIds: favorisIds ? favorisIds.filter(f => f !== source.id) : [],
+          favoris: favoris ? favoris.filter(f => f.id !== source.id) : []
+        },
+        refetchQueries
+      });
+    }
+
+    return client.mutate({
+      mutation: ADD_TO_FAVORIS,
+      variables: {
+        userId,
+        favorisIds: [...favorisIds, source.id],
+        favoris: favoris ? [...favoris, source] : [source]
+      },
+      refetchQueries
+    });
+  };
+
+  const isFavoris = data.user.favorisIds.includes(source.id);
+
+  if (buttonWithIcon) {
+    return (
+      <Button
+        icon={isFavoris ? 'favorite' : 'favorite-border'}
+        color={isFavoris ? '#EE05F2' : '#607D8B'}
+        size={20}
+        uppercase={false}
+        animated
+        onPress={AddOrRemoveToFavoris}>
+        Favoris
+      </Button>
+    );
+  }
+
+  return (
+    <IconButton
+      icon={isFavoris ? 'favorite' : 'favorite-border'}
+      color={isFavoris ? '#EE05F2' : '#607D8B'}
+      size={25}
+      onPress={AddOrRemoveToFavoris}
+      animated
+    />
+  );
+};
+
+Favoris.defaultProps = {
+  buttonWithIcon: false
+};
+
+export default withApollo<FavorisProps>(Favoris);
--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -1,10 +1,20 @@
+import { Dimensions } from 'react-native';
 import { connect } from '../../store';
 import Sidebar from '../../components/Sidebar';
 
-const SidebarContainer = connect(({ playerIsOpened, source, userId }) => ({
-  playerIsOpened,
-  source,
-  userId
-}))(Sidebar);
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const FROM_VALUE = WINDOW_WIDTH;
+const TO_VALUE = 0;
+
+const SidebarContainer = connect(({ playerIsOpened, source }) => {
+  const from = !source ? FROM_VALUE : playerIsOpened ? FROM_VALUE : TO_VALUE;
+  const to = playerIsOpened ? TO_VALUE : FROM_VALUE;
+
+  return {
+    playerIsOpened,
+    from,
+    to
+  };
+})(Sidebar);
 
 export default SidebarContainer;
