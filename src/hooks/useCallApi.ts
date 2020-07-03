0e888431a95b141617f352166aa1bb77c587660a
Stéphane <contact@stephane-richin.fr>
2020-07-03 23:26:31 +0200

    :bug: Fixing first instance call

--- src/hooks/useCallApi.ts
+++ src/hooks/useCallApi.ts
@@ -1,12 +1,14 @@
 import useFetch from 'fetch-suspense';
 import useStore from './useStore';
 import { SearchVideo } from '../types';
+import { PUBLIC_INVIDIOUS_INSTANCES } from '../constants';
 
 const useCallApi = (url: string): SearchVideo[] => {
   const store = useStore();
-  const data = useFetch(`${store.instance}/api/v1/${url}`);
+  const data = useFetch(
+    `${store.instance ?? PUBLIC_INVIDIOUS_INSTANCES[0].value}/api/v1/${url}`
+  );
 
-  // @ts-ignore
   return data;
 };
 
