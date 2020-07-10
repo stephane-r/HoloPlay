61811778bfd4fbcf74178c387ba54fb01fe5184b
Stéphane <contact@stephane-richin.fr>
2020-07-10 13:54:39 +0200

    :white_check_mark: Adding logs for test code-push deploy

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const App = () => {
   useEffect(() => {
+    console.log('test');
     if (process.env.NODE_ENV === 'production') {
       codePush.sync({
         updateDialog: true,
