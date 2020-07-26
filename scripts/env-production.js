448357a8ba14f55927970659fdd625a115b0e38a
Stéphane <contact@stephane-richin.fr>
2020-07-26 18:05:24 +0200

    :bug: Fixing missing env on travis-ci

--- scripts/env-production.js
+++ scripts/env-production.js
@@ -4,6 +4,10 @@ const envs = [
   {
     value: 'SENTRY_DSN_KEY',
     property: process.env.SENTRY_DSN_KEY
+  },
+  {
+    value: 'YOUTUBE_AUDIO_SERVER_API_URL',
+    property: process.env.YOUTUBE_AUDIO_SERVER_API_URL
   }
 ];
 
