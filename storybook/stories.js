ab25087c47276b5550daf0e21454ff383f23367e
Stéphane <contact@stephane-richin.fr>
2020-08-10 09:36:00 +0200

    :heavy_minus_sign: Removing storybook :(

--- storybook/stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react-native';
-import {
-  CardVerticalStory,
-  CardHorizontalStory
-} from '../src/components/Card/Layout/index.story';
-
-storiesOf('Card', module).add('Horizontal', () => <CardHorizontalStory />);
-storiesOf('Card', module).add('Vertical', () => <CardVerticalStory />);
