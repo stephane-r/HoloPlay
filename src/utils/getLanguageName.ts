11dcd3e5b2ef1a4b83490ad9cdbcace074e0e480
Fjuro <fjuro@seznam.cz>
2022-01-31 11:19:38 +0100

    Add Czech to getLanguageName.ts

--- src/utils/getLanguageName.ts
+++ src/utils/getLanguageName.ts
@@ -1,9 +1,12 @@
-const getLanguageName = (lng: 'en' | 'fr'): string => {
+const getLanguageName = (lng: 'en' | 'fr' | 'cs'): string => {
   let language;
 
   switch (true) {
     case lng === 'fr':
       language = 'Français';
+      break;
+    case lng === 'cs':
+      language = 'Čeština';
       break;
     default:
       language = 'English';
